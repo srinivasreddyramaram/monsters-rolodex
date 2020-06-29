7ea72e5ce460a3921d6c5aa5210b003fcb1ff5f6
SriRam <sramaram@rythmos.com>
2020-06-29 14:22:08 +0530

    render a list of monsters uniquely identified by a key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,20 +6,33 @@ class App extends Component {
   constructor() {
     super();
     this.state =  {
-      name: 'SriRam'
+      monsters: [
+        {
+          id: 1,
+          name: 'Banshee'
+        },
+        {
+          id: 2,
+          name: 'Troll'
+        },
+        {
+          id: 3,
+          name: 'Dwarf'
+        },
+        {
+          id: 4,
+          name: 'Frankenstein'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello {this.state.name}
-          </p>
-          <button onClick={() => this.setState({name:'Phani'})}>Change name</button>
-        </header>
+        {this.state.monsters.map((monster) => (
+        <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
