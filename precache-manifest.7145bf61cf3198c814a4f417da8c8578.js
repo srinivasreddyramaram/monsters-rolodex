5e57e031252d55562675fb593e69bf90c11e1d7b
SriRam <sramaram@rythmos.com>
2020-06-30 18:24:59 +0530

    Updates

---
+++ precache-manifest.7145bf61cf3198c814a4f417da8c8578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6cc81006fb75efe7fc016ac942de3f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ae0e4afc6d2ee5e7e473",
+    "url": "/monsters-rolodex/static/css/main.f50f44ef.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0e4afc6d2ee5e7e473",
+    "url": "/monsters-rolodex/static/js/main.e3737ef9.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
